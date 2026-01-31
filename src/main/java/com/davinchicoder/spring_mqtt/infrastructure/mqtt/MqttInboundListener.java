ef6526221085c07150f77f8f633834f8cefc064e
David <david11jv@gmail.com>
2026-01-31 12:52:30 +0100

    Initial commit

---
+++ src/main/java/com/davinchicoder/spring_mqtt/infrastructure/mqtt/MqttInboundListener.java
@@ -0,0 +1,31 @@
+package com.davinchicoder.spring_mqtt.infrastructure.mqtt;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class MqttInboundListener {
+
+
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public void handle(Message<String> message) {
+
+        String payload = message.getPayload();
+        String topic = message.getHeaders()
+                .get(MqttHeaders.RECEIVED_TOPIC, String.class);
+
+        if (topic == null) {
+            log.error("No topic found in message: {}", payload);
+            return;
+        }
+
+        TelemetryTopic t = TelemetryTopic.from(topic);
+    }
+}
+
---
+++ src/main/java/com/davinchicoder/spring_mqtt/infrastructure/mqtt/TelemetryTopic.java
@@ -0,0 +1,12 @@
+package com.davinchicoder.spring_mqtt.infrastructure.mqtt;
+
+public record TelemetryTopic(
+        String tenantId,
+        String robotId
+) {
+    public static TelemetryTopic from(String topic) {
+        String[] p = topic.split("/");
+        return new TelemetryTopic(p[1], p[2]);
+    }
+}
+
